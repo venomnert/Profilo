7ed6cee9bfa420f08db5f92524e7774ed6a3ab85
venomnert <neerthigan@gmail.com>
2019-03-06 23:11:06 -0500

    removed unused file

--- profilo/assets/js/Root.js
+++ profilo/assets/js/Root.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header'
-import HomePage from './pages'
 
 export default class Root extends React.Component {
   render() {
