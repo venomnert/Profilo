1a2408644b7f0479af8e1c4b3bf1ee4a09deb0cc
venomnert <neerthigan@gmail.com>
2019-03-06 22:52:29 -0500

    added router path for react entry

--- profilo/lib/profilo_web/router.ex
+++ profilo/lib/profilo_web/router.ex
@@ -16,7 +16,7 @@ defmodule ProfiloWeb.Router do
   scope "/", ProfiloWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
