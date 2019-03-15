6cb79f3e2ab585ae2445d8279d3b27fe7cabda31
venomnert <neerthigan@gmail.com>
2019-03-15 15:10:50 -0400

    testing redeployment

--- assets/js/Root.js
+++ assets/js/Root.js
@@ -2,11 +2,16 @@ import * as React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header'
+import TestHeader from './components/Test_Header'
+
 
 export default class Root extends React.Component {
   render() {
     return (
+      <div>
         <Header />
-    )
+        <TestHeader />
+      </div>
+    )        
   }
 }
---
+++ assets/js/components/Test_Header.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Header = () => (
+  <header>
+    <h1>Another Header Test</h1>
+  </header>
+)
+
+export default TestHeader
