2168d696504063a3b9a3af2d07a417007694d491
venomnert <neerthigan@gmail.com>
2019-03-16 16:06:40 -0400

    Trying to fix redirect_mismatch error from github

--- lib/profilo_web/controllers/auth_controller.ex
+++ lib/profilo_web/controllers/auth_controller.ex
@@ -17,17 +17,10 @@ defmodule ProfiloWeb.AuthController do
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    case UserFromAuth.find_or_create(auth) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "Successfully authenticated.")
-        |> put_session(:current_user, user)
-        |> redirect(to: "/")
-      {:error, reason} ->
-        conn
-        |> put_flash(:error, reason)
-        |> redirect(to: "/")
-    end
+      conn
+      |> put_flash(:info, "Successfully authenticated.")
+      # |> put_session(:current_user, user)
+      |> redirect(to: "/api/users")
   end
 
 end
