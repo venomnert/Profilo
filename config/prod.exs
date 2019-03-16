bdf7ed6b415de51976decb3cbb45e7423666b3f4
venomnert <neerthigan@gmail.com>
2019-03-16 10:28:42 -0400

    Fixed pool size

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :profilo, Profilo.Repo,
   url: "${DATABASE_URL}",
   database: "",
   ssl: true,
-  pool_size: 1
+  pool_size: 2
 
 # Do not print debug messages in production
 config :logger, level: :info
