5c807efb71f70bb22133309c3c858ebc73474680
Nert <Ner@Nerts-MacBook-Pro.local>
2019-07-13 12:22:32 -0400

    Removed unused modules

--- lib/profilo_web/plug/absinthe_context.ex
+++ lib/profilo_web/plug/absinthe_context.ex
@@ -1,12 +1,6 @@
 defmodule ProfiloWeb.Plug.AbsintheContext do
   @behaviour Plug
 
-  import Plug.Conn
-  import Ecto.Query, only: [where: 2]
-
-  alias Profilo.Repo
-  alias Profilo.Accounts.Lib.User
-
   def init(opts), do: opts
 
   def call(conn, _) do
