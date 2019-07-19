e5e748b50038062f9309991874bf8ceb28c08bfc
Nert <Ner@Nerts-MacBook-Pro.local>
2019-07-19 08:39:00 -0400

    Added association between social_link and feed_node

--- lib/profilo/entity/lib/social_link.ex
+++ lib/profilo/entity/lib/social_link.ex
@@ -2,12 +2,16 @@ defmodule Profilo.Entity.Lib.SocialLink do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Profilo.Entity.Lib.{Following, SocialLink}
+  alias Profilo.Entity.Lib.{Following, SocialLink, FeedNode}
 
   schema "social_link" do
     field :name, :string
 
     has_many :following, Following,
+      on_delete: :delete_all,
+      foreign_key: :social_link_id
+
+    has_many :feed_node, FeedNode,
       on_delete: :delete_all,
       foreign_key: :social_link_id
 
