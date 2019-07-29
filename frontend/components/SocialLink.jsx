7162a57f6faa46fbab4a5475f4e4a0ec47cb8fb8
Nert <Ner@Nerts-MacBook-Pro.local>
2019-07-29 08:10:51 -0400

    Render socia_link component via id or name

--- frontend/components/SocialLink.jsx
+++ frontend/components/SocialLink.jsx
@@ -5,7 +5,7 @@ import GithubIcon from '../static/icons/Github.svg';
 
 class SocialLink extends Component {
     render() {
-        if(this.props.name === "github") return <GithubIcon width="40px" height="40px" className="fill-primary"/>;
+        if(this.props.name === "github" || this.props.id == "1") return <GithubIcon width="40px" height="40px" className="fill-primary"/>;
         return <TwitterIcon width="40px" height="40px" className="fill-primary" />
     }
 }
